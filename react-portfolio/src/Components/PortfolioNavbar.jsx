729a976955d7b476f8492d2a44e3c0daaeda62a3
Zach Smith <zachsmith852@gmail.com>
2024-03-22 23:25:41 -0700

    Add nav structure

---
+++ react-portfolio/src/Components/PortfolioNavbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export function PortfolioNavbar() {
+  return (
+  <>
+  <header className="flex">
+        <h1>Zachary Smith</h1>
+        <ul>
+          <li>About Me</li>
+          <li> Work</li>
+          <li>Contact Me</li>
+          <li>Resume</li>
+        </ul>
+      </header>
+      </>
+      )
+}
