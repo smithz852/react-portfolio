b9af7996fe73040b029dc36bea8b59ee4ed5ae96
Zach Smith <zachsmith852@gmail.com>
2024-03-22 22:23:59 -0700

    Add html to convert to React

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,35 +1,84 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <header className="flex">
+        <h1>Zachary Smith</h1>
+        <ul>
+          <li><a href="#About-Me"> About Me</a></li>
+          <li><a href="#Work"> Work</a></li>
+          <li><a href="#Contact-Me"> Contact Me</a></li>
+          <li><a href="./Assets/Media/Zachary Smith - Resume 2023.pdf"> Resume</a></li>
+        </ul>
+      </header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="flex">
+          <img id="portrait" src='./assets/IMG_1304' alt="Picture of Zach" />
+          <div>
+            <h2 id="About-Me" style={{width: "400px"}}>About Me</h2>
+            <div className="bio-text">
+              <p>I'm currently pursuing a full stack web development certification through UCLA, with a planned completion
+                date of April 11, 2024.
+                Prior to this, I graduated in 2021 with a Bachelor of Science in Business Management from CSULB. Prior to,
+                during, and after graduating, I've gained experience in various industries.
+                This includes music marketing, Google Adwords marketing, hospitality, restaurant project management, and the
+                automotive industry. </p>
+              <p>I enjoy working creatively and am constantly striving to further my knowledge so I may achieve my goals.
+                I'm excited about the potential a web development career provides
+                and cannot wait to begin this journey! </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex work-sect">
+      <h2 id="Work" className="work-sidebar">Work</h2>
+      <div className="work-flexbox">
+        <div className="container">
+          <a href="./Assets/HTMLs/ComingSoon.html"> <img src="./assets/102_50-c.jpg" className="card myButton"/></a>
+          <div className="card-placeholder card-descr">
+            <span className="project-descr">Placeholder Project:</span> <span className="project-text">Content TBD</span>
+          </div>
+        </div>
+        <div className="container">
+          <a href="./Assets/HTMLs/ComingSoon.html"> <img src="./Assets/Media/GOPR1678.jpg" className="myButton card"/> </a>
+          <div className="card-placeholder card-descr"><span className="project-descr">Placeholder Project:</span> <span
+              className="project-text">Content TBD</span></div>
+        </div>
+        <div className="container">
+          <a href="./Assets/HTMLs/ComingSoon.html"> <img src="./Assets/Media/GOPR1678.jpg" className="card myButton"/> </a>
+          <div className="card-placeholder card-descr"><span className="project-descr">Placeholder Project:</span> <span
+              className="project-text">Content TBD</span></div>
+        </div>
+        <div className="container">
+          <a href="./Assets/HTMLs/ComingSoon.html"> <img src="./Assets/Media/102_50-c.jpg" className="card myButton"/></a>
+          <div className="card-placeholder card-descr"> <span className="project-descr">Placeholder Project:</span> <span
+              className="project-text">Content TBD</span></div>
+        </div>
+        <div id="top-container">
+          <a href="https://smithz852.github.io/01-module/"><img src="./Assets/Media/Screenshot 2023-10-30 154641.png"
+              id="top-card" className="myButton"/></a>
+          <div className="card-top top-descr"><span className="project-descr">Accessibility Optimization:</span> <span
+              className="project-text">HTML & CSS</span></div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+<footer>
+  <div className="footer-block">
+    <h3 id="Contact-Me">Contact Me</h3>
+    <ul className="contact-me">
+      <li><a href="Tel: 805-760-3230"> (805) 760-3230</a></li>
+      <li><a href="mailto:zachsmith852@gmail.com"> Email</a></li>
+      <li><a href="https://github.com/smithz852"> Github</a></li>
+      <li><a href="https://www.linkedin.com/in/zachary-smith-610a32178/"> LinkedIn</a></li>
+    </ul>
+  </div>
+</footer>
     </>
-  )
+  );
 }
+
+ 
 
 export default App
