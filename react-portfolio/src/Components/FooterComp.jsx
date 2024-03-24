9357b43c8efe05c2d5ad453a4cb2000635793100
Zach Smith <zachsmith852@gmail.com>
2024-03-23 23:27:08 -0700

    Adjust footer size and flex area

--- react-portfolio/src/Components/FooterComp.jsx
+++ react-portfolio/src/Components/FooterComp.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 
 export function FooterComp() {
   return (
-    <footer>
-  <div className="footer-block">
-    <h3 id="Contact-Me">Contact Me</h3>
-    <ul className="contact-me">
-      <li>(805) 760-3230</li>
-      <li>Email</li>
-      <li> Github</li>
-      <li>LinkedIn</li>
-    </ul>
-  </div>
-</footer>
-  )
+    <>
+    <header className="flex footerMargin">
+          <h2>Contact Me</h2>
+          <ul>
+            <li>(805) 760-3230</li>
+            <li> Email</li>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+          </ul>
+        </header>
+        </>
+        )
 }
