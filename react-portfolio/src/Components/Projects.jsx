3656e3d7111ee21b398fc2a72b59a237a0d5c25e
Zach Smith <zachsmith852@gmail.com>
2024-03-24 16:59:59 -0700

    Add links to carousel on home page

--- react-portfolio/src/Components/Projects.jsx
+++ react-portfolio/src/Components/Projects.jsx
@@ -16,19 +16,25 @@ export function Projects() {
     <div className="carousel-container">
     <Carousel>
       <Carousel.Item>
+        <a href="https://github.com/smithz852/recipe-finder.git">
         <img src={recipeFinder} alt="Picture of recipe finder website" />
+        </a>
         <Carousel.Caption>
           <h3>Recipe Finder</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <a href="https://github.com/KyleGru/sports-squares.git">
         <img src={sportSquares} alt="Picture of sport squares app" />
+        </a>
         <Carousel.Caption>
           <h3>Sport Squares</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <a href="https://github.com/smithz852/ecommerce-app.git">
         <img src={ecommApi} alt="Image of ecommerce api" />
+        </a>
         <Carousel.Caption>
           <h3>eCommerce API</h3>
         </Carousel.Caption>
