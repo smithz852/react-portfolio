786de08b8b02bfd150accc211bcd87da0360f6c0
Zach Smith <zachsmith852@gmail.com>
2024-03-24 21:19:21 -0700

    Add contact page and links

--- react-portfolio/src/Components/FooterComp.jsx
+++ react-portfolio/src/Components/FooterComp.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Link } from 'react-router-dom'
 
 export function FooterComp() {
+
+
   return (
     <>
     <header className="flex footerMargin">
           <h2>Contact Me</h2>
           <ul>
-            <li>(805) 760-3230</li>
-            <li> Email</li>
-            <li>GitHub</li>
-            <li>LinkedIn</li>
+            <li><a href="tel: +18057603230">(805) 760-3230</a></li>
+            <li><a href="mailto: zachsmith852@gmail.com">Email</a></li>
+            <li><a href="https://github.com/smithz852">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/zachary-smith-610a32178/">LinkedIn</a></li>
           </ul>
         </header>
         </>
